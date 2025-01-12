70568991456993779c559c5670906cdbbd9d5128
BipanaPokharel <your-email@example.com>
2025-01-12 23:07:54 +0545

    Initial commit for frontend

--- lib/features/presentation/signup/signup.dart
+++ lib/features/presentation/signup/signup.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:fyp/features/presentation/login/login.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class SignUp extends StatefulWidget {
   const SignUp({super.key});
@@ -17,6 +19,7 @@ class _SignUpState extends State<SignUp> {
   bool _obscureConfirmPassword = true;
   DateTime? _dateOfBirth;
   String? _gender;
+
   final TextEditingController _dobController = TextEditingController();
   final TextEditingController _name = TextEditingController();
   final TextEditingController _email = TextEditingController();
@@ -24,13 +27,60 @@ class _SignUpState extends State<SignUp> {
   final TextEditingController _confirmPassword = TextEditingController();
   final TextEditingController _phoneNumber = TextEditingController();
 
+  Future<void> signup() async {
+    if (_formKey.currentState!.validate()) {
+      final response = await http.post(
+        Uri.parse(
+            'http://localhost:3000/api/signup'), // Change this to your actual endpoint
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode({
+          'username': _name.text,
+          'email': _email.text,
+          'password': _password.text,
+          'profile_pic':
+              'url_to_profile_pic', // Optional, change as per your logic
+          'fitness_goal': 'Your fitness goal here', // Optional
+          'phone_number': _phoneNumber.text, // Optional, added phone number
+          'dob': _dateOfBirth != null
+              ? DateFormat('yyyy-MM-dd').format(_dateOfBirth!) // Format date
+              : null,
+          'gender': _gender,
+        }),
+      );
+
+      if (response.statusCode == 201) {
+        // Handle successful signup
+        final responseData = json.decode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+              content: Text('Signup successful: ${responseData['message']}')),
+        );
+        // Navigate to the Login page after successful signup
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+              builder: (context) =>
+                  Login()), // Replace with your login page widget
+        );
+      } else {
+        // Handle error response
+        final errorData = json.decode(response.body);
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Signup failed: ${errorData['message']}')),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFFf5f5f5),
+      backgroundColor: Colors.amber[800],
       appBar: AppBar(
         title: const Text('Sign Up'),
-        backgroundColor: const Color(0xFF2196F3),
+        backgroundColor: Colors.amber[800],
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
@@ -43,12 +93,10 @@ class _SignUpState extends State<SignUp> {
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: _email,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Email',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.email),
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -64,20 +112,14 @@ class _SignUpState extends State<SignUp> {
                 const SizedBox(height: 20),
                 TextFormField(
                   controller: _name,
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Full Name',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    prefixIcon: const Icon(Icons.person_4_outlined),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.person_4_outlined),
                   ),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your name';
-                    }
-                    final usernameRegex = RegExp(r'^[a-zA-Z]+$');
-                    if (!usernameRegex.hasMatch(value)) {
-                      return 'Please enter a valid username with letters only';
                     }
                     if (value.length < 5) {
                       return 'Name should be at least 5 characters';
@@ -90,33 +132,27 @@ class _SignUpState extends State<SignUp> {
                   controller: _phoneNumber,
                   maxLength: 10,
                   inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-                  decoration: InputDecoration(
-                    labelText: 'Phone Number',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    prefixIcon: const Icon(Icons.phone),
-                    counterText: "",
-                  ),
+                  decoration: const InputDecoration(
+                      labelText: 'Phone Number',
+                      border: OutlineInputBorder(),
+                      prefixIcon: Icon(Icons.phone),
+                      counterText: ""),
                   validator: (value) {
                     if (value == null || value.isEmpty) {
                       return 'Please enter your phone number';
                     }
-
                     if (value.length < 10) {
-                      return 'Phone number should be at least 10 digits';
+                      return "Phone number should be at least 10 digits";
                     }
                     return null;
                   },
                 ),
                 const SizedBox(height: 20),
                 DropdownButtonFormField<String>(
-                  decoration: InputDecoration(
+                  decoration: const InputDecoration(
                     labelText: 'Select Gender',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    prefixIcon: const Icon(Icons.transgender_sharp),
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.transgender_sharp),
                   ),
                   value: _gender,
                   items: const [
@@ -157,12 +193,10 @@ class _SignUpState extends State<SignUp> {
                     child: TextFormField(
                       controller: _dobController,
                       readOnly: true,
-                      decoration: InputDecoration(
+                      decoration: const InputDecoration(
                         labelText: 'Date of Birth (yyyy-MM-dd)',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
-                        prefixIcon: const Icon(Icons.calendar_today),
+                        border: OutlineInputBorder(),
+                        prefixIcon: Icon(Icons.calendar_today),
                       ),
                       validator: (value) {
                         if (value == null || value.isEmpty) {
@@ -179,16 +213,12 @@ class _SignUpState extends State<SignUp> {
                   obscureText: _obscurePassword,
                   decoration: InputDecoration(
                     labelText: 'Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                    border: const OutlineInputBorder(),
                     prefixIcon: const Icon(Icons.lock),
                     suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscurePassword
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                      ),
+                      icon: Icon(_obscurePassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
                       onPressed: () {
                         setState(() {
                           _obscurePassword = !_obscurePassword;
@@ -211,16 +241,12 @@ class _SignUpState extends State<SignUp> {
                   controller: _confirmPassword,
                   decoration: InputDecoration(
                     labelText: 'Confirm Password',
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
+                    border: const OutlineInputBorder(),
                     prefixIcon: const Icon(Icons.lock),
                     suffixIcon: IconButton(
-                      icon: Icon(
-                        _obscureConfirmPassword
-                            ? Icons.visibility_off
-                            : Icons.visibility,
-                      ),
+                      icon: Icon(_obscureConfirmPassword
+                          ? Icons.visibility_off
+                          : Icons.visibility),
                       onPressed: () {
                         setState(() {
                           _obscureConfirmPassword = !_obscureConfirmPassword;
@@ -238,50 +264,15 @@ class _SignUpState extends State<SignUp> {
                   },
                 ),
                 const SizedBox(height: 20),
-                Row(
-                  children: [
-                    Radio<bool>(
-                      value: true,
-                      groupValue: _rememberMe,
-                      onChanged: (bool? value) {
-                        setState(() {
-                          _rememberMe = value;
-                        });
-                      },
-                    ),
-                    const Text('Remember me'),
-                  ],
-                ),
-                const SizedBox(height: 20),
                 ElevatedButton(
-                  onPressed: () {
-                    if (_formKey.currentState!.validate()) {
-                      // Handle sign-up logic 
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text("Fill all the fields")),
-                      );
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor:
-                        const Color(0xFF2196F3), 
-                  ),
+                  onPressed: signup, // Call the signup function when pressed
                   child: const Text('Sign Up'),
                 ),
-                const SizedBox(height: 20),
               ],
             ),
           ),
         ),
       ),
     );
-  }
-
-  @override
-  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
-    super.debugFillProperties(properties);
-    properties
-        .add(DiagnosticsProperty<DateTime?>('_dateOfBirth', _dateOfBirth));
   }
 }
